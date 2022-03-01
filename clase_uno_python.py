fbcdb32db40970a15c14afeab605c3a0b042808d
Deyner Castro <dcastro31@cuc.edu.co>
2022-02-28 21:34:15 -0500

    Variables, operaciones, tipos de datos 1

---
+++ clase_uno_python.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Variables
+
+a = 3
+print(type(a))
+a = "Hola"
+print(type(a))
+a = 3.5
+print(type(a))
+a = True
+print(type(a))
+# Type es utiñizado para saber el tipo de dato utilizado
+
+print('Hola mundo')
+# Print es para immprimir en consola y se usa mucho la comilla simple
+
+# Operaciones
+
+# Suma
+a = 5
+b = 2
+c = a + b
+print(c)
+
+# Resta
+a = 5
+b = 2
+c = a - b
+print(c)
+
+# Multiplicacion
+a = 5
+b = 2
+c = a * b
+print(c)
+
+# Division 
+a = 5
+b = 2
+c = a / b
+print(c)
+
+# Division parte entera
+a = 5
+b = 2
+c = a // b
+print(c)
+
+# Potencia
+a = 2
+c = a ** 6
+print(c)
+
+# Raiz cuadrada
+a = 16
+b = a ** (1/2)
+print(b)
+
+import math
+raiz = math.sqrt(a)
+print(raiz)
+
+# Raiz cubica
+a = 16
+b = a ** (1/3)
+print(b)
+
+# Tipos de datos
+
+#
